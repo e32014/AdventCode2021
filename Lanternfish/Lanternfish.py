4daff3c4883cfd4f556e294d44029d52d32c7e22
Evan Klei <e32014@gmail.com>
2021-12-06 17:30:35 -0500

    Day 6: Simplified answer for day 6 to be a daily iterator over a weekly one

--- Lanternfish/Lanternfish.py
+++ Lanternfish/Lanternfish.py
@@ -3,27 +3,18 @@ from copy import deepcopy
 file = open("input.txt")
 
 fishes = [int(val) for val in file.readline().strip().split(",")]
-periods = [0] * 9
+today = [0] * 9
 for fish in fishes:
-    periods[fish] = periods[fish] + 1
-fishCount = len(fishes)
+    today[fish] = today[fish] + 1
+
 days = 256
-weeks = days // 7
-additional = days % 7
-for _ in range(weeks):
-    nextWeek = [0] * 9
-    sevens = periods[7]
-    eights = periods[8]
-    periods[7] = 0
-    periods[8] = 0
-    for i in range(len(periods)):
-        nextWeek[(i + 2) % 9] = periods[(i + 2) % 9] + periods[i]
-    nextWeek[0] = nextWeek[0] + sevens
-    nextWeek[1] = nextWeek[1] + eights
-    periods = deepcopy(nextWeek)
+for _ in range(days):
+    tomorrow = [0] * 9
+    for i in range(7):
+        tomorrow[i] = today[(i + 1) % 7]
+    tomorrow[8] = today[0]
+    tomorrow[7] = today[8]
+    tomorrow[6] += today[7]
+    today = deepcopy(tomorrow)
 
-addon = 0
-for i in range(additional):
-    addon += periods[i]
-
-print(sum(periods) + addon)
+print(sum(today))
